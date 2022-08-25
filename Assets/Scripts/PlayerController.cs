95c0e6731792a8d755e3ad0766dd68d6e44b4525
Michael <mikedoherty2119@gmail.com>
2022-08-24 21:51:33 -0400

    implemented readUserInput in PC

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,17 +9,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed = 5.0f;
     [SerializeField] float jumpForce = 5.0f;
     [SerializeField] Animator animator;
+    [SerializeField] private LayerMask jumpableGround;
     
     private Rigidbody2D body;
+    private BoxCollider2D coll;
 
     /// <summary>
     /// Player input, left or right, between -1 and 1
     /// </summary>
     private float moveDirection;
-    /// <summary>
-    /// True if currently jumping, false if not
-    /// </summary>
-    private bool isJumping = false;
     /// <summary>
     /// True if facing right, false if facing left
     /// </summary>
@@ -30,42 +28,48 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         readUserInput();
-        if (body.velocity.y == 0)
-        {
-            isJumping = false;
-        }
-    }
-
-    /// <summary>
-    /// Process player input, pass data to animator to
-    /// animate movement, and flip direction of player if needed
-    /// </summary>
-    private void readUserInput()
-    {
-        // Read and process movement
-        moveDirection = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(moveDirection * moveSpeed, body.velocity.y);
-        animator.SetFloat("Speed", Mathf.Abs(moveDirection));
-        if (Input.GetButtonDown("Jump") && !(isJumping))
-        {
-            body.velocity = new Vector2(body.velocity.x, jumpForce);
-            isJumping = true;
-        }
 
         // Flip character model if needed
         if (moveDirection < 0 && isFacingRight){flipCharacter();}
         if (moveDirection == 0 && !isFacingRight){flipCharacter();}
     }
 
+    /// <summary>
+    /// Process player input, pass data to animator
+    /// </summary>
+    private void readUserInput()
+    {
+        // Horizontal input
+        moveDirection = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(moveDirection * moveSpeed, body.velocity.y);
+        
+        // Vertical input
+        if (Input.GetButtonDown("Jump") && IsGrounded()){jump();}
+
+        //Pass data to animator       
+        animator.SetFloat("Speed", Mathf.Abs(moveDirection));
+    }
+
+    /************************* Function Definitions *************************/
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
     private void flipCharacter()
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0,180f,0f);
     }
+    private void jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpForce);
+    }
+    /********************** End Function Definitions **********************/
 }
